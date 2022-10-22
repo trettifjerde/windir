6b0c97cd94f42900656db12621d7ad246db3e23d
trettifjerde <sasjandreeva@gmail.com>
2022-10-22 15:30:23 +0000

    static version upgrade

--- windir_site/settings.py
+++ windir_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('WINDIR_SC')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'a3-windir.club']
 
